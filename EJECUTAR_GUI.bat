b736b36dd45b464ef865b13e38352b53976632e7
Nicolas <nicolasguardattiutn@gmail.com>
2025-11-09 19:47:53 -0300

    .

--- EJECUTAR_GUI.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo   SIMULACION DE BIBLIOTECA - GUI
-echo ========================================
-echo.
-echo Iniciando interfaz grafica...
-echo.
-python gui_biblioteca.py
-pause
